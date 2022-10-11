df226739a233f4af57799346e079e2914ba6a5c1
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2022-10-12 01:39:20 +1000

    approuting added to all pages

--- part2/src/app/app-routing.module.ts
+++ part2/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GroupassisComponent } from './groupassis/groupassis.component';
+import { GroupadminComponent } from './groupadmin/groupadmin.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SuperadminComponent } from './superadmin/superadmin.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path:'login', component:LoginComponent},
+                        {path:'groupassis', component:GroupassisComponent},
+                        {path:'profile', component:ProfileComponent},
+                        {path:'logout', component:LogoutComponent},
+                        {path:'superadmin', component:SuperadminComponent},
+                        {path:'groupadmin', component:GroupadminComponent},
+
+                      ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- part2/src/app/app.module.ts
+++ part2/src/app/app.module.ts
@@ -1,28 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { GroupassisComponent } from './groupassis/groupassis.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LogoutComponent } from './logout/logout.component';
 import { SuperadminComponent } from './superadmin/superadmin.component';
 import { GroupadminComponent } from './groupadmin/groupadmin.component';
-import { GroupassisComponent } from './groupassis/groupassis.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    GroupassisComponent,
     ProfileComponent,
     LogoutComponent,
     SuperadminComponent,
-    GroupadminComponent,
-    GroupassisComponent
+    GroupadminComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
