c3019a6303ca8879634119d511ce6366e1c7a680
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2022-10-14 23:31:27 +1000

    updated object types

--- part2/src/app/channel.ts
+++ part2/src/app/channel.ts
@@ -2,6 +2,6 @@ import { Userobj } from "./userobj"
 import { Chatobj } from "./chat"
 export interface Channelobj {
     Channelname: String
-    Userlist : [Userobj]
-    chatList:[Chatobj]
+    Userlist : []
+    chatList:[]
 }
--- part2/src/app/groupobj.ts
+++ part2/src/app/groupobj.ts
@@ -2,6 +2,6 @@ import { Userobj } from "./userobj";
 import { Channelobj } from "./channel";
 export interface Groupobj {
     Groupname: String
-    userlist : [Userobj]
-    Channellist: [Channelobj]
+    userlist : []
+    Channellist: []
 }
