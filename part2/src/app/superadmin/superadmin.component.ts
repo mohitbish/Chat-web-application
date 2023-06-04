e3b619537873edb4d038b8e2cc396583e8643ac1
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-04 20:05:50 +1000

    suderadmin pages edits

--- part2/src/app/superadmin/superadmin.component.ts
+++ part2/src/app/superadmin/superadmin.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Userobj } from '../userobj';
+import { Groupobj } from '../groupobj';
+import { Channelobj } from '../channel';
+import { Chatobj } from '../chat';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
+const BACKEND_URL = 'http://localhost:3000';
+// for angular http methods
+
 
 @Component({
   selector: 'app-superadmin',
@@ -9,9 +21,26 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class SuperadminComponent implements OnInit {
 
-  constructor(private router:Router,  private httpClient: HttpClient) { }
+
+  Groups: Groupobj[] = []
+  constructor(private router:Router, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.getGroups();
+  }
+
+  getGroups(){
+    this.httpClient.get<Groupobj[]>(BACKEND_URL + '/getgroups')
+      .subscribe((data:any)=>{
+        this.Groups = data;
+        console.log(typeof(data), data);
+      })
+  }
+
+  opengroup(Group: Groupobj){
+    localStorage.clear
+    localStorage.setItem('Group', JSON.stringify(Group));
+    this.router.navigateByUrl("/groupview");
   }
 
 }
