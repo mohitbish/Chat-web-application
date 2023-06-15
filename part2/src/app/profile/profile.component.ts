50d1e839414058b8264cac2efce379a973a0d983
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-16 03:51:31 +1000

    formatiing :)

--- part2/src/app/profile/profile.component.ts
+++ part2/src/app/profile/profile.component.ts
@@ -1,44 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 // const BACKEND_URL = 'http://localhost:3000';
 const BACKEND_URL = 'https://localhost:3000';
 
 // for angular http methods
 import { NgForm } from '@angular/forms';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 import { Userobj } from '../userobj';
-
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  
-  constructor(private router: Router, private httpClient: HttpClient) { 
-    console.log(sessionStorage.getItem('Role'))
+  constructor(private router: Router, private httpClient: HttpClient) {
+    console.log(sessionStorage.getItem('Role'));
     //checks for login status
-    if (!(sessionStorage.getItem('Loginstatus')=="true")){
-      alert("login please");
-      this.router.navigateByUrl("/login");
+    if (!(sessionStorage.getItem('Loginstatus') == 'true')) {
+      alert('login please');
+      this.router.navigateByUrl('/login');
     }
     //redirect to views according to Role
-    if ((sessionStorage.getItem('Loginstatus')=="true") && (sessionStorage.getItem('Role')=="superadmin") ){
-      this.router.navigateByUrl("/superadmin");
+    if (
+      sessionStorage.getItem('Loginstatus') == 'true' &&
+      sessionStorage.getItem('Role') == 'superadmin'
+    ) {
+      this.router.navigateByUrl('/superadmin');
     }
-    if ((sessionStorage.getItem('Loginstatus')=="true") && (sessionStorage.getItem('Role')=="user") ){
-      this.router.navigateByUrl("/groupassis");
+    if (
+      sessionStorage.getItem('Loginstatus') == 'true' &&
+      sessionStorage.getItem('Role') == 'user'
+    ) {
+      this.router.navigateByUrl('/groupassis');
     }
   }
 
-  ngOnInit(): void {
-   
-  }
-
-
+  ngOnInit(): void {}
 }
