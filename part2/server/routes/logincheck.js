9682c4ff7f7892f491cbd84d4a09994a30b3dda9
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-19 02:31:35 +1000

    work on user and messaging

--- part2/server/routes/logincheck.js
+++ part2/server/routes/logincheck.js
@@ -1,36 +1,33 @@
-var fs = require('fs');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+var fs = require("fs");
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 
 // Connection URL
-const url = 'mongodb://0.0.0.0:27017/';
+const url = "mongodb://0.0.0.0:27017/";
 
 // Database Name
-const dbName = 'mydb';
+const dbName = "mydb";
 
 // Create a new MongoClient
 const client = new MongoClient(url);
 
-module.exports = function(req, res) {
-    // Get the documents collection
-    const db = client.db(dbName);
-    const collection = db.collection('Users');
-    // Find some documents
-    collection.find({Username: req.body.Username, Password: req.body.Password}).toArray(function(err, user) {
+module.exports = function (req, res) {
+  // Get the documents collection
+  const db = client.db(dbName);
+  const collection = db.collection("Users");
+  // Find some documents
+  collection
+    .find({ Username: req.body.Username, Password: req.body.Password })
+    .toArray(function (err, user) {
       assert.equal(err, null);
-      console.log(user)
-      if(err){
-        console.log(err)
+      console.log(user);
+      if (err) {
+        console.log(err);
       }
-      if(user[0] == [ ]){
-        console.log(err)
+      if (user[0] == []) {
+        console.log(err);
+      } else {
+        res.send({ ok: true, user: user[0] });
       }
-      else{
-        res.send({"ok":true, "user":user[0]});
-      }
-      
-      
     });
-    
-}
-
+};
