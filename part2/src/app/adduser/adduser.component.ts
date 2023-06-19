9e5cf0714dde88b725472c6d569c14c3dcfe0f9a
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-06-20 03:41:00 +1000

    channelview css added

--- part2/src/app/adduser/adduser.component.ts
+++ part2/src/app/adduser/adduser.component.ts
@@ -1,43 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Userobj } from '../userobj';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 const BACKEND_URL = 'http://localhost:3000';
 // for angular http methods
 
-
 @Component({
   selector: 'app-adduser',
   templateUrl: './adduser.component.html',
-  styleUrls: ['./adduser.component.css']
+  styleUrls: ['./adduser.component.css'],
 })
 export class AdduserComponent implements OnInit {
-    Username : String = ""
-    Password: String = ""
-    Email : String = ""
-    Role: String = ""
+  Username: String = '';
+  Password: String = '';
+  Email: String = '';
+  Role: String = '';
 
-  constructor(private router:Router, private httpClient: HttpClient) { }
+  constructor(private router: Router, private httpClient: HttpClient) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   // add new user to database
-  adduser(){
-    let user = {Username: this.Username, Email : this.Email, Role: this.Role, Password: this.Password}
-    this.httpClient.post(BACKEND_URL + '/adduser', user, httpOptions)
-      .subscribe((data:any)=>{
-        console.log(data.user);
-        if(data.ok){
-          alert("added");
-          this.router.navigateByUrl("/users");
-        }
-       
-      })
+  adduser() {
+    if (
+      this.Username == '' ||
+      this.Password == '' ||
+      this.Role == '' ||
+      this.Email == ''
+    ) {
+      alert('FORM NOT FILLED');
+    } else {
+      let user = {
+        Username: this.Username,
+        Email: this.Email,
+        Role: this.Role,
+        Password: this.Password,
+      };
+      this.httpClient
+        .post(BACKEND_URL + '/adduser', user, httpOptions)
+        .subscribe((data: any) => {
+          console.log(data.user);
+          if (data.ok) {
+            alert('added');
+            this.router.navigateByUrl('/users');
+          }
+        });
+    }
   }
-
 }
